5e66c081e44e56e542b75a32f43fb82fd0060db5
panncherry <maung.eainmatmay@gmail.com>
2018-10-18 17:43:23 -0400

    added HomeViewController

---
+++ EatSpector/EatSpector/ViewControllers/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HomeViewController.swift
+//  EatSpector
+//
+//  Created by Pann Cherry on 10/18/18.
+//  Copyright © 2018 TechBloomer. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //custom navigation bar
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBar.barTintColor = UIColor(red: 255, green: 0/255, blue: 0/255, alpha: 1)
+        self.navigationController?.navigationBar.tintColor = UIColor.white
+        self.navigationController!.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+    }
+
+}
