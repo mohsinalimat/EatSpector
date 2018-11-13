008b7e3d10260d5269363413aa4fdd8c7b7a9699
panncherry <maung.eainmatmay@gmail.com>
2018-11-13 18:26:49 -0500

    seamless button

--- EatSpector/EatSpector/ViewControllers/SeamLessViewController.swift
+++ EatSpector/EatSpector/ViewControllers/SeamLessViewController.swift
@@ -7,24 +7,46 @@
 //
 
 import UIKit
+import WebKit
+import CoreLocation
 
-class SeamLessViewController: UIViewController {
+class SeamLessViewController: UIViewController, WKUIDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var SeamlessOrderWebView: WKWebView!
+    
+    let locationManager = CLLocationManager()
+
+    var business: Business?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+        
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+        
+        if UIApplication.shared.canOpenURL(URL(string: "https://www.google.com/maps/search/pizza")!) {
+            // Map is installed. Launch Map and get direction
+           // let urlStr = String(format: "maps://place/" + business!.name)
+           // UIApplication.shared.openURL(URL(string: "maps://place/pizza")!)
+            UIApplication.shared.openURL(URL(string: "https://www.google.com/maps/search/pizza")!)
+        } else {
+            // Map is not installed. Launch AppStore to install Map app
+            
+            UIApplication.shared.openURL(URL(string: "https://www.google.com/maps/place/pizza")!)
+        }
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
     }
-    */
-
 }
