25e9e088433b3d84d1bf788212779e6a3a8eecaa
panncherry <maung.eainmatmay@gmail.com>
2018-11-13 18:23:37 -0500

    seamless button added

---
+++ EatSpector/EatSpector/ViewControllers/SeamLessViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SeamLessViewController.swift
+//  EatSpector
+//
+//  Created by Pann Cherry on 11/10/18.
+//  Copyright © 2018 TechBloomer. All rights reserved.
+//
+
+import UIKit
+
+class SeamLessViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
