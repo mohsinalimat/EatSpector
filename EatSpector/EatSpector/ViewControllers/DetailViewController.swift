ea4d5933910fd9c18417a79811e84d1bafc81409
panncherry <maung.eainmatmay@gmail.com>
2018-11-22 01:16:43 -0500

    updated deailviewcontroller

--- EatSpector/EatSpector/ViewControllers/DetailViewController.swift
+++ EatSpector/EatSpector/ViewControllers/DetailViewController.swift
@@ -11,17 +11,16 @@ import CoreLocation
 
 
 class DetailViewController: UIViewController, CLLocationManagerDelegate {
-
+    
     @IBOutlet weak var busniessImg: UIImageView!
     @IBOutlet weak var businessNameLabel: UILabel!
-    @IBOutlet weak var categoriesLabel: UILabel!
     @IBOutlet weak var gradingLabel: UILabel!
     @IBOutlet weak var record_dateLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var phoneLabel: UILabel!
     
     let locationManager = CLLocationManager()
-
+    
     var business: Business?
     
     override func viewDidLoad() {
@@ -29,21 +28,21 @@ class DetailViewController: UIViewController, CLLocationManagerDelegate {
         
         if let business = business {
             businessNameLabel.text = business.name
-            gradingLabel.text = business.grading
-            categoriesLabel.text = business.categories
-            record_dateLabel.text = business.record_date
-            let building = business.building_number + " "
-            let street = business.street + ", "
-            let boro = business.boro + ", "
-            let state = "NY "
-            let zip = business.zipcode + " "
-            addressLabel.text = building+street+boro+state+zip
+            gradingLabel.text = "Grade: " + business.grading
+            record_dateLabel.text = "Record date: " + business.record_date
+            addressLabel.text = formatAddress(strBuilding: business.building_number, strStreet: business.street, strBoro: business.boro, strZip: business.zipcode)
             phoneLabel.text = "Tel: " + arrangeUSFormat(strPhone: business.phone)
+        }
     }
+    
+    //code to format address
+    func formatAddress(strBuilding : String, strStreet : String, strBoro : String, strZip : String)-> String{
+        var strUpdated = strBuilding + " " + strStreet + ", " + strBoro + ", NY " + strZip
+        return strUpdated
     }
     
     
-    //code to format the telephone string
+    //code to format phone number
     func arrangeUSFormat(strPhone : String)-> String {
         var strUpdated = strPhone
         if strPhone.characters.count == 10 {
@@ -55,24 +54,25 @@ class DetailViewController: UIViewController, CLLocationManagerDelegate {
         return strUpdated
     }
     
+    //code to go to google map for direction
     @IBAction func getDirectionButton(_ sender: Any) {
         if let business = business {
             let street = business.street
             let trimedStreet = street.replacingOccurrences(of: " ", with: "+")
             let address = business.building_number + "+" + trimedStreet + "+" + business.boro + "+" + business.zipcode
             let baseURLString = "https://www.google.com/maps/search/"
-        
+            
             if let url = URL(string: baseURLString + address) {
                 UIApplication.shared.open(url, options: [:])
             }
         }
     }
-
-   /* override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    
+    /* override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
      
      let trailerViewController = segue.destination as! SeamLessViewController
-        trailerViewController.business = business
-        
-    }*/
-
+     trailerViewController.business = business
+     
+     }*/
+    
 }
